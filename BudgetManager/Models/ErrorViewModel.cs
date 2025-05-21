997ce2e444ffe0a71ef6648d25395143138b3507
cjohn281 <cjohn281@outlook.com>
2025-05-20 19:12:12 -0700

    Initial commit

---
+++ BudgetManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BudgetManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
