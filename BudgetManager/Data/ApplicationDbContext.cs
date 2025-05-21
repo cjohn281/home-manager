ecdcd3a27ff5d42e2bdc337c4bdf029b1c424005
cjohn281 <cjohn281@outlook.com>
2025-05-21 13:07:09 -0700

    add ApplicationDbContext class

---
+++ BudgetManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BudgetManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
+
