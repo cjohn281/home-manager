758def117c4f4111fad64b50407d5936fb6d7bdc
cjohn281 <cjohn281@outlook.com>
2025-05-21 20:36:54 -0700

    update

--- BudgetManager/Data/ApplicationDbContext.cs
+++ BudgetManager/Data/ApplicationDbContext.cs
@@ -9,8 +9,15 @@ namespace BudgetManager.Data
             : base(options)
         { }
 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Incidental> Incidentals { get; set; }
+        public DbSet<IncidentalItem> IncidentalItems { get; set; }
+        public DbSet<Ledger> Ledgers { get; set; }
+        public DbSet<LedgerBalance> LedgerBalances { get; set; }
+        public DbSet<LedgerItem> LedgerItems { get; set; }
         public DbSet<Person> Persons { get; set; }
-
+        public DbSet<RecurringItem> RecurringItems { get; set; }
+        public DbSet<TransactionType> TransactionTypes { get; set; }
     }
 }
 
