c2f17c8fa8c744aa6fd47571f445fd461c0a816a
cjohn281 <cjohn281@outlook.com>
2025-05-21 18:14:19 -0700

    add controller for managing user login/logout

---
+++ BudgetManager/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using BudgetManager.Data;
+using BudgetManager.Helpers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BudgetManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AccountController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var test = _dbContext.Database.GetDbConnection().ConnectionString;
+            var user = await _dbContext.Persons.FirstOrDefaultAsync(p => p.Email == email);
+            if (user == null || !PasswordHelper.VerifyPassword(password, user.Salt, user.Password))
+            {
+                ModelState.AddModelError("", "Invalid email or password");
+                return View();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Name),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddDays(30)
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+    }
+}
