0ebf8dc3ab42219b975cc69087a5fd2da59fb5be
cjohn281 <cjohn281@outlook.com>
2025-08-13 19:13:55 -0700

    Add IncomeDetail page

--- home-manager/Areas/BudgetManager/Models/IncomeDetail.cs
+++ home-manager/Areas/BudgetManager/Models/IncomeDetail.cs
@@ -1,33 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace home_manager.Areas.BudgetManager.Models
+﻿namespace home_manager.Areas.BudgetManager.Models
 {
-    [Table("tbl_income_detail")]
     public class IncomeDetail
     {
-        [Column("icd_id")]
         public int Id { get; set; } = 0;
-
-        [Column("icd_person_prn_id")]
         public int Person_prnId { get; set; } = 0;
-
         public string PersonName { get; set; } = string.Empty;
-
-        [Column("icd_default_amount")]
         public decimal DefaultAmount { get; set; } = 0.0M;
-
-        [Column("icd_lookup_value_lvl_id")]
-        public int LookupValue_lvlId { get; set; } = 0;
-
+        public int PayFrequency_lvlId { get; set; } = 0;
         public string PayFrequency { get; set; } = string.Empty;
-
-        [Column("icd_extended_value1")]
         public int? PayDate1 { get; set; }
-
-        [Column("icd_extended_value2")]
         public int? PayDate2 { get; set; }
-
-        [Column("icd_first_pay")]
         public DateTime? ValidDate { get; set; }
     }
 }
---
+++ home-manager/Areas/BudgetManager/ViewModels/LookupValue_VModel.cs
@@ -0,0 +1,12 @@
+﻿namespace home_manager.Areas.BudgetManager.ViewModels
+{
+    public class LookupValue_VModel
+    {
+        public int Id { get; set; } = 0;
+        public int LookupType_lktId { get; set; } = 0;
+        public string LookupTypeName { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public string? ExtendedValue1 { get; set; }
+        public string? ExtendedValue2 { get; set; }
+    }
+}
